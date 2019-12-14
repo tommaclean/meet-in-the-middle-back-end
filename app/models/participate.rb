141116ab361e225a7d2c621b552a1f1d687a3a95
tommaclean <tom.j.maclean@gmail.com>
2019-12-14 17:59:36 -0500

    fixed Participate class error

--- app/models/participate.rb
+++ app/models/participate.rb
@@ -1,4 +1,4 @@
 class Participate < ApplicationRecord
-  belongs_to :participant1, class_name: User
-  belongs_to :participant2, class_name: User
+  belongs_to :participant1, class_name: 'User'
+  belongs_to :participant2, class_name: 'User'
 end
