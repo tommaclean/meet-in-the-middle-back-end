2ed211ed676f0dd531bde79924569c63460f42ca
tommaclean <tom.j.maclean@gmail.com>
2019-12-15 15:34:04 -0500

    added skip_before_action :verify_authenticity_token

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,7 +2,7 @@ class AuthController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     is_authenticated = user.authenticate(params[:password]) if user
-
+    skip_before_action :verify_authenticity_token
     if is_authenticated
       # byebug
       render json: { token: encode_token(user) }
