c93e81d08ed3d092b296cd5b2528a9ef9049e0f4
tommaclean <tom.j.maclean@gmail.com>
2019-12-15 13:27:21 -0500

    added index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :user_meetups
   resources :users
 
-  get '/', to: 'users#index'
+  get '/', to: 'home#index'
   post '/login', to: 'auth#create'
   post '/signup', to: 'users#create'
   get '/profile', to: 'users#profile'
